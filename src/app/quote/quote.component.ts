c60ca07b798ab1006f4db109cabee9faa50dad89
Github User Name <you@example.com>
2018-10-21 19:47:39 +0300

    dislike button works but loops across we need to select individual quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,31 +9,32 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
 
-  numberOfLikes:number = 0;
-  
-  likeButtonClick(){
+  numberOfLikes: number = 0;
+  numberOfDislikes: number = 0;
+
+  likeButtonClick() {
     this.numberOfLikes++;
 
   }
 
-  dislikeButtonClick(){
-    this.numberOfLikes--;
+  dislikeButtonClick() {
+    this.numberOfDislikes++;
   }
 
 
   quotes = [
-    new Quote(1, '“When one is pretending the entire body revolts.”', 'By -Anaïs Nin', new Date(2019, 3, 14) ),
-    new Quote(2, '“Be there for others, but never leave yourself behind.”', 'By -Dodinsky', new Date(2019, 6, 9)  ),
-    new Quote(3, '“Do what you must,And your friends will adjust.”', 'By -Robert Brault', new Date(2019, 1, 12) ),
+    new Quote(1, '“When one is pretending the entire body revolts.”', 'By -Anaïs Nin', new Date(2019, 3, 14)),
+    new Quote(2, '“Be there for others, but never leave yourself behind.”', 'By -Dodinsky', new Date(2019, 6, 9)),
+    new Quote(3, '“Do what you must,And your friends will adjust.”', 'By -Robert Brault', new Date(2019, 1, 12)),
   ]
 
-  addNewQuote(quote){
+  addNewQuote(quote) {
     let quoteLength = this.quotes.length;
-    quote.id=quoteLength+1;
+    quote.id = quoteLength + 1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
 
-}
+  }
 
 
   toogleDetails(index) {
